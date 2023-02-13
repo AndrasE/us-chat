224cfaa6cf8fffe3f7ceb1a66dfe26e509a451b8
Andras E <rohadtsajt@gmail.com>
2023-02-13 18:58:03 +0000

    navigation flow vol 1

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, createContext, useContext, useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { onAuthStateChanged } from "firebase/auth";
 
 import Chat from "./screens/Chat";
 import Login from "./screens/Login";
@@ -8,13 +10,32 @@ import Signup from "./screens/Signup";
 import Home from "./screens/Home";
 
 const Stack = createStackNavigator();
+const AuthenticationUserContext = createContext({});
+
+const AuthenticationUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <AuthenticationUserContext.Provider value={{user, setUser}}>
+       { children }
+       </AuthenticationUserContext.Provider>
+  )
+}
+
 
 function ChatStack () {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Signup" component={Signup} />
+    <Stack.Navigator defaultScreenOptions={Home}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Chat" component={Chat} />
     </Stack.Navigator>
   )
+}
+
+function AuthStack () {
+  <Stack.Navigator defaultScreenOptions={Login}>
+  <Stack.Screen name="Login" component={Login} />
+  <Stack.Screen name="Signup" component={Signup} />
+</Stack.Navigator>
 }
 
 function RootNavigator () {
